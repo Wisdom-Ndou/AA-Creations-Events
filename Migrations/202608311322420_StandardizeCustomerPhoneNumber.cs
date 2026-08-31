69599aa3349cad006a5ae3c6884e3f7eee4a5ec6
Wizzy <wisdomdou@gmail.com>
2026-08-31 20:41:26 +0200

    Added manage account section, fix a few other bugs as well

---
+++ Migrations/202608311322420_StandardizeCustomerPhoneNumber.cs
@@ -0,0 +1,18 @@
+﻿namespace WebApplication1.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class StandardizeCustomerPhoneNumber : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Customers", "Cust_Phone", c => c.String(nullable: false, maxLength: 9));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Customers", "Cust_Phone", c => c.String(nullable: false));
+        }
+    }
+}
